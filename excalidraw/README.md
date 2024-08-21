9c9d31c204f0addac99fc3c6676e230a92adda42
Folusho Oladipo <fdoladipo@gmail.com>
2024-08-21 23:44:08 +0100

    Add code from Excalidraw that I will review

---
+++ excalidraw/README.md
@@ -0,0 +1,5 @@
+## Name and Short Description
+Excalidraw: according to their repo, this is "an open source virtual hand-drawn style whiteboard. ".
+
+## Repository
+https://github.com/excalidraw/excalidraw/
---
+++ excalidraw/actionAddToLibrary.ts
@@ -0,0 +1,64 @@
+// Source: https://github.com/excalidraw/excalidraw/blob/b5d7f5b4ba85db38aa2c13df88c68d258ae70828/packages/excalidraw/actions/actionAddToLibrary.ts#L1
+import { register } from "./register";
+import { deepCopyElement } from "../element/newElement";
+import { randomId } from "../random";
+import { t } from "../i18n";
+import { LIBRARY_DISABLED_TYPES } from "../constants";
+import { StoreAction } from "../store";
+
+export const actionAddToLibrary = register({
+  name: "addToLibrary",
+  trackEvent: { category: "element" },
+  perform: (elements, appState, _, app) => {
+    const selectedElements = app.scene.getSelectedElements({
+      selectedElementIds: appState.selectedElementIds,
+      includeBoundTextElement: true,
+      includeElementsInFrames: true,
+    });
+
+    for (const type of LIBRARY_DISABLED_TYPES) {
+      if (selectedElements.some((element) => element.type === type)) {
+        return {
+          storeAction: StoreAction.NONE,
+          appState: {
+            ...appState,
+            errorMessage: t(`errors.libraryElementTypeError.${type}`),
+          },
+        };
+      }
+    }
+
+    return app.library
+      .getLatestLibrary()
+      .then((items) => {
+        return app.library.setLibrary([
+          {
+            id: randomId(),
+            status: "unpublished",
+            elements: selectedElements.map(deepCopyElement),
+            created: Date.now(),
+          },
+          ...items,
+        ]);
+      })
+      .then(() => {
+        return {
+          storeAction: StoreAction.NONE,
+          appState: {
+            ...appState,
+            toast: { message: t("toast.addedToLibrary") },
+          },
+        };
+      })
+      .catch((error) => {
+        return {
+          storeAction: StoreAction.NONE,
+          appState: {
+            ...appState,
+            errorMessage: error.message,
+          },
+        };
+      });
+  },
+  label: "labels.addToLibrary",
+});
